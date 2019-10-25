0ea52cbd576969dcc104699abfd68c3ba84e5c7f
JAPM <cicelcup@gmail.com>
2019-10-25 10:30:14 -0600

    including jet brain annotations and changing to english language (all the spanish annotations)

--- app/src/main/java/com/example/android/countcounter/MainActivity.java
+++ app/src/main/java/com/example/android/countcounter/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import org.jetbrains.annotations.NotNull;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
-    int puntuacionEquipo1;
-    int puntuacionEquipo2;
+    int scoreTeam1;
+    int scoreTeam2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,52 +20,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setToZero(){
-        puntuacionEquipo1 = 0;
-        puntuacionEquipo2 = 0;
-        mostrarPuntuacionEquipo(puntuacionEquipo1, (TextView) findViewById(R.id.label_puntuacion_equipo1));
-        mostrarPuntuacionEquipo(puntuacionEquipo2, (TextView) findViewById(R.id.label_puntuacion_equipo2));
+        scoreTeam1 = 0;
+        scoreTeam2 = 0;
+        showScore(scoreTeam1, (TextView) findViewById(
+                R.id.label_team_1));
+        showScore(scoreTeam2, (TextView) findViewById(
+                R.id.label_team2));
 
     }
-    private void mostrarPuntuacionEquipo(int x, TextView y) {
-        y.setText("" + x);
+    private void showScore(int x, @NotNull TextView y) {
+        y.setText(String.format(Locale.getDefault(),"%d",x));
     }
 
     public void reset(View view){
         setToZero();
     }
 
-    public void Incrementar_Puntuacion(View view){
-        String cualBoton = getResources().getResourceEntryName(view.getId());
+    public void increaseScore(@NotNull View view){
+        String whichButton = getResources().getResourceEntryName(view.getId());
 
-        switch (cualBoton){
-            case "btn_1punto_equipo1":
-                puntuacionEquipo1 = puntuacionEquipo1 +1;
-                mostrarPuntuacionEquipo(puntuacionEquipo1, (TextView) findViewById(R.id.label_puntuacion_equipo1));
+        switch (whichButton){
+            case "btn_1point_team1":
+                scoreTeam1 = scoreTeam1 +1;
+                showScore(scoreTeam1, (TextView) findViewById(R.id.label_team_1));
                 break;
-            case "btn_2puntos_equipo1":
-                puntuacionEquipo1 = puntuacionEquipo1 +2;
-                mostrarPuntuacionEquipo(puntuacionEquipo1, (TextView) findViewById(R.id.label_puntuacion_equipo1));
+            case "btn_2points_team1":
+                scoreTeam1 = scoreTeam1 +2;
+                showScore(scoreTeam1, (TextView) findViewById(R.id.label_team_1));
                 break;
-            case "btn_3puntos_equipo1":
-                puntuacionEquipo1 = puntuacionEquipo1 +3;
-                mostrarPuntuacionEquipo(puntuacionEquipo1, (TextView) findViewById(R.id.label_puntuacion_equipo1));
+            case "btn_3points_team1":
+                scoreTeam1 = scoreTeam1 +3;
+                showScore(scoreTeam1, (TextView) findViewById(R.id.label_team_1));
                 break;
-            case "btn_1punto_equipo2":
-                puntuacionEquipo2 = puntuacionEquipo2 +1;
-                mostrarPuntuacionEquipo(puntuacionEquipo2, (TextView) findViewById(R.id.label_puntuacion_equipo2));
+            case "btn_1point_team2":
+                scoreTeam2 = scoreTeam2 +1;
+                showScore(scoreTeam2, (TextView) findViewById(R.id.label_team2));
                 break;
-            case "btn_2puntos_equipo2":
-                puntuacionEquipo2 = puntuacionEquipo2 +2;
-                mostrarPuntuacionEquipo(puntuacionEquipo2, (TextView) findViewById(R.id.label_puntuacion_equipo2));
+            case "btn_2points_team2":
+                scoreTeam2 = scoreTeam2 +2;
+                showScore(scoreTeam2, (TextView) findViewById(R.id.label_team2));
                 break;
-            case "btn_3puntos_equipo2":
-                puntuacionEquipo2 = puntuacionEquipo2 +3;
-                mostrarPuntuacionEquipo(puntuacionEquipo2, (TextView) findViewById(R.id.label_puntuacion_equipo2));
+            case "btn_3points_team2":
+                scoreTeam2 = scoreTeam2 +3;
+                showScore(scoreTeam2, (TextView) findViewById(R.id.label_team2));
                 break;
         }
 
     }
-
-
-
 }
